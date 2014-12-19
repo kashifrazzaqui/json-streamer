d007f069181489c3e2ca78afac08cfad4d7d0528
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2014-12-19 15:41:54 +0530

    add module docstring to __init__

--- jsonstreamer/__init__.py
+++ jsonstreamer/__init__.py
@@ -1,1 +1,9 @@
+"""jsonstreamer provides a SAX-like push parser via the JSONStreamer class and a 'object' parser via the
+class which emits top level entities in any JSON object.
+
+Useful for parsing partial JSON coming over the wire or via disk
+Uses 'again' python module's 'events.EventSource' framework for event boilerplate
+again -> https://github.com/kashifrazzaqui/again#eventing-boilerplate
+"""
+
 from jsonstreamer.jsonstreamer import JSONStreamer, ObjectStreamer
