4331e49be564e262ae692c76844bb6865fb9a81d
Kashif Razzaqui <kashifrazzaqui@Kashif-Primary-2.local>
2025-10-17 23:28:36 +0100

    Fix ruff errors: exclude notebooks and update setup.py
    - Exclude README.ipynb from ruff checking (old notebook format)
    - Update setup.py to v2.0.0 with cffi dependency
    - Fix line length in setup.py description
    
    All ruff checks now pass.

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-setup(name='jsonstreamer',
-      version='1.3.6',
-      author='Kashif Razzaqui',
-      author_email='kashif.razzaqui@gmail.com',
-      url='https://github.com/kashifrazzaqui/json-streamer',
-      description='Provides a SAX-like push parser which works with partial json fragments. '
-                  'Also provides an ObjectStreamer that emits key-value pairs or array elements of the `root` json object/array.'
-                  'Based on the fast c libary yajl',
-      packages=['jsonstreamer', 'jsonstreamer.yajl'],
-      install_requires=['again']
-      )
+setup(
+    name="jsonstreamer",
+    version="2.0.0",
+    author="Kashif Razzaqui",
+    author_email="kashif.razzaqui@gmail.com",
+    url="https://github.com/kashifrazzaqui/json-streamer",
+    description=(
+        "Provides a SAX-like push parser which works with partial json fragments. "
+        "Also provides an ObjectStreamer that emits key-value pairs or array elements "
+        "of the `root` json object/array. Based on the fast c library yajl."
+    ),
+    packages=["jsonstreamer", "jsonstreamer.yajl"],
+    install_requires=["cffi>=1.15.0"],
+)
